6366a5d535f2b911cc56b94548b0afeede515038
sitiqotijah <sitiqotijah01@gmail.com>
2020-03-30 21:44:20 +0700

    Updates

---
+++ precache-manifest.809cd36ae2728d6f741d18dcb669bb18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069946e837c5e1330ee9fdb37f99a561",
+    "url": "/sitiq.github.io/WEB3A-431743-Siti-Qotijah-REACT/index.html"
+  },
+  {
+    "revision": "6e96f1a1aad729ad5db7",
+    "url": "/sitiq.github.io/WEB3A-431743-Siti-Qotijah-REACT/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "b4a5a421f55dcc5574bb",
+    "url": "/sitiq.github.io/WEB3A-431743-Siti-Qotijah-REACT/static/css/main.4733d224.chunk.css"
+  },
+  {
+    "revision": "6e96f1a1aad729ad5db7",
+    "url": "/sitiq.github.io/WEB3A-431743-Siti-Qotijah-REACT/static/js/2.56429058.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/sitiq.github.io/WEB3A-431743-Siti-Qotijah-REACT/static/js/2.56429058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a5a421f55dcc5574bb",
+    "url": "/sitiq.github.io/WEB3A-431743-Siti-Qotijah-REACT/static/js/main.f83af060.chunk.js"
+  },
+  {
+    "revision": "277053aadf4b9e87484d",
+    "url": "/sitiq.github.io/WEB3A-431743-Siti-Qotijah-REACT/static/js/runtime-main.38575127.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sitiq.github.io/WEB3A-431743-Siti-Qotijah-REACT/static/media/logo.5d5d9eef.svg"
+  }
+]);
